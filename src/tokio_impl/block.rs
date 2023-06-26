939ca517a0edf2021ee9065e559df13ec8d42d44
Radu <radu.racariu@gmail.com>
2023-06-26 14:18:13 -0700

    Implement basic conversion logic

--- src/tokio_impl/block.rs
+++ src/tokio_impl/block.rs
@@ -2,9 +2,12 @@
 
 use std::time::Duration;
 
+use anyhow::Result;
 use futures::future::select_all;
 use futures::FutureExt;
+use libhaystack::encoding::zinc;
 use libhaystack::val::kind::HaystackKind;
+use libhaystack::val::{Bool, Number, Str, Value};
 
 use crate::base::{block::Block, input::input_reader::InputReader};
 use crate::blocks::utils::sleep_millis;
@@ -18,11 +21,11 @@ impl<B: Block> InputReader for B {
         let millis = timeout.as_millis() as u64;
         let _ = select_all([
             sleep_millis(millis).boxed_local(),
-            (async move || {
+            async {
                 if self.read_inputs().await.is_none() {
                     sleep_millis(millis).await;
                 };
-            })()
+            }
             .boxed_local(),
         ])
         .await;
@@ -61,8 +64,17 @@ pub(crate) async fn read_block_inputs<B: Block>(block: &mut B) -> Option<usize> 
 
     if let Some(value) = val {
         if let Some(input) = inputs.get_mut(idx) {
-            if *input.kind() != HaystackKind::from(&value) {
-                block.set_state(crate::base::block::BlockState::Fault);
+            let expected = *input.kind();
+            let actual = HaystackKind::from(&value);
+
+            if expected != actual {
+                match convert_value(value, expected, actual) {
+                    Ok(value) => input.set_value(value),
+                    Err(err) => {
+                        log::error!("Error converting value: {}", err);
+                        block.set_state(crate::base::block::BlockState::Fault);
+                    }
+                }
             } else {
                 input.set_value(value);
             }
@@ -72,5 +84,67 @@ pub(crate) async fn read_block_inputs<B: Block>(block: &mut B) -> Option<usize> 
         Some(idx)
     } else {
         None
+    }
+}
+
+/// Converts a value from one kind to another.
+///
+/// # Arguments
+///	- `val` - The value to convert
+/// - `expected` - The expected kind of the value
+/// - `actual` - The actual kind of the value
+///
+/// # Returns
+/// The converted value if the conversion was successful.
+fn convert_value(val: Value, expected: HaystackKind, actual: HaystackKind) -> Result<Value> {
+    match (expected, actual) {
+        (HaystackKind::Bool, HaystackKind::Bool) => Ok(val),
+        (HaystackKind::Bool, HaystackKind::Number) => {
+            let val = Number::try_from(&val).map_err(|err| anyhow::anyhow!(err))?;
+
+            Ok((val.value != 0.0).into())
+        }
+        (HaystackKind::Bool, HaystackKind::Str) => {
+            let val = Str::try_from(&val).map_err(|err| anyhow::anyhow!(err))?;
+
+            let num = zinc::decode::from_str(&val.value)?;
+            if num.is_bool() {
+                Ok(num)
+            } else {
+                Err(anyhow::anyhow!("Expected a bool value, but got {:?}", val))
+            }
+        }
+
+        (HaystackKind::Number, HaystackKind::Number) => Ok(val),
+        (HaystackKind::Number, HaystackKind::Bool) => {
+            let val = Bool::try_from(&val).map_err(|err| anyhow::anyhow!(err))?;
+
+            Ok((if val.value { 1 } else { 0 }).into())
+        }
+        (HaystackKind::Number, HaystackKind::Str) => {
+            let val = Str::try_from(&val).map_err(|err| anyhow::anyhow!(err))?;
+
+            let num = zinc::decode::from_str(&val.value)?;
+            if num.is_number() {
+                Ok(num)
+            } else {
+                Err(anyhow::anyhow!(
+                    "Expected a number value, but got {:?}",
+                    val
+                ))
+            }
+        }
+
+        (HaystackKind::Str, HaystackKind::Str) => Ok(val),
+        (HaystackKind::Str, HaystackKind::Bool) => Ok(val.to_string().as_str().into()),
+        (HaystackKind::Str, HaystackKind::Number) => {
+            let str = zinc::encode::to_zinc_string(&val)?;
+            Ok(str.as_str().into())
+        }
+        _ => Err(anyhow::anyhow!(
+            "Cannot convert {:?} to {:?}",
+            actual,
+            expected
+        )),
     }
 }
