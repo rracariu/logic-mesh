4864e658d85b1de4516e99d5e7f5e133f7952694
Radu <radu.racariu@gmail.com>
2023-08-25 09:28:33 -0700

    Add place holder for run program, copy, paste programs.

--- web/src/components/ToolBar.vue
+++ web/src/components/ToolBar.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue';
 
 import Toolbar from 'primevue/toolbar';
 import Button from 'primevue/button';
+import Dropdown from 'primevue/dropdown';
 
 import { command } from '../lib/Engine';
 
@@ -32,12 +33,19 @@ const onReset = () => {
 
 <template>
 	<Toolbar>
+		<template #start>
+			<Dropdown optionLabel="programName" placeholder="Select a Program" class="w-full md:w-14rem" disabled />
+		</template>
 		<template #center>
 			<Button title="Pause/Resume execution" :icon="'pi ' + (isRunning ? 'pi-pause' : 'pi-play')" rounded
 				@click="onPauseResume" />
 		</template>
 		<template #end>
-			<Button title="Reset engine" :icon="'pi pi-refresh'" rounded @click="onReset" />
+			<div class="flex-grow">
+				<Button title="Copy program" :icon="'pi pi-copy'" class="m-1" disabled />
+				<Button title="Paste program" :icon="'pi pi-file'" class="m-1" disabled />
+				<Button title="Reset engine" :icon="'pi pi-refresh'" rounded @click="onReset" class="m-1" />
+			</div>
 		</template>
 	</Toolbar>
 </template>
