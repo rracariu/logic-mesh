bbc39d3b25305ef50c00beb8d2323f7e22c1eb72
Radu <radu.racariu@gmail.com>
2026-03-08 13:50:24 +0200

    Restructure the web app folders.

--- build-wasm.sh
+++ build-wasm.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-wasm-pack build --dev --out-dir ./web/module/pkg
-mv ./web/module/pkg/*.{js,ts,wasm} ./web/module
-cd ./web/module && npm version $(node -p "require('./pkg/package.json').version") --allow-same-version && rm -rf ./pkg
+cd "$(dirname "$0")/web"
+npm run build:dev --workspace=logic-mesh
